36d7fb5c86bb7c499975baa523d91e48f065da5a
LuisCarBl <134304449+LuisCarBl@users.noreply.github.com>
2023-08-20 21:10:52 +0000

    pal backend

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -22,9 +22,9 @@ public class HomeController {
         return "index";
     }
 
-    @GetMapping(path = "/api/bandas/{id}/formacion")
+    @GetMapping(path = "/api/studios/{id}/formacion")
     public @ResponseBody List<Map<String, Object>> formacion(@PathVariable Integer id){
-        String sql = "SELECT integrante.id as ID, musico.nombre as MUSICO, instrumento.nombre as INSTRUMENTO FROM integrante JOIN musico ON integrante.id_musico = musico.id JOIN instrumento ON integrante.id_instrumento = instrumento.id WHERE integrante.id_banda = ?";
+        String sql = "SELECT integrante.id as ID, director.nombre as DIRECTOR, pelicula.nombre as PELICULA FROM integrante JOIN director ON integrante.id_director = director.id JOIN pelicula ON integrante.id_pelicula = pelicula.id WHERE integrante.id_studio = ?";
         List<Map<String, Object>> queryResult = jdbcTemplate.queryForList(sql, id);
         return queryResult;
     }
