04741cd75f02c1a99e8ae8a691c5625afbc9fd3f
LuisCarBl <134304449+LuisCarBl@users.noreply.github.com>
2023-08-20 21:06:29 +0000

    pal backend

--- src/main/java/com/example/demo/DatabaseLoader.java
+++ src/main/java/com/example/demo/DatabaseLoader.java
@@ -28,18 +28,18 @@ public class DatabaseLoader implements CommandLineRunner {
 	@Override
 	public void run(String... strings) throws Exception {
 
-		Pelicula pBlueBeetle = new Pelicula("Blue Beetle", "Acción");
-		Pelicula pEscapeBajoFuego = new Pelicula("Escape Bajo Fuego", "Acción");
+		Pelicula pBlueBeetle = new Pelicula("Blue Beetle", "Accion");
+		Pelicula pEscapeBajoFuego = new Pelicula("Escape Bajo Fuego", "Accion");
 		Pelicula pHospitalSangriento = new Pelicula("Hospital Sangriento", "Terror");
-		this.repositoryP.save(new Pelicula("Transformers: El despertar de las Bestias", "Ciencia Ficci\u00F3n"));
-		this.repositoryP.save(new Pelicula("Super Mario Bros. La película","Aventura"));
-		this.repositoryP.save(new Pelicula("Megalodon 2","Acción"));
-		this.repositoryP.save(new Pelicula("Elementos", "Animación"));
+		this.repositoryP.save(new Pelicula("Transformers: El despertar de las Bestias", "Ciencia Ficcion"));
+		this.repositoryP.save(new Pelicula("Super Mario Bros. La pelicula","Aventura"));
+		this.repositoryP.save(new Pelicula("Megalodon 2","Accion"));
+		this.repositoryP.save(new Pelicula("Elementos", "Accion"));
 		this.repositoryP.save(pBlueBeetle);
-		this.repositoryP.save(pEscapeBajoFuego);
 		this.repositoryP.save(pHospitalSangriento);
+		this.repositoryP.save(pEscapeBajoFuego);
 
-		Director dAngelManuel = new Director("Ángel Manuel Soto");
+		Director dAngelManuel = new Director("Angel Manuel Soto");
 		Director dRicRoman = new Director("Ric Roman Waugh");
 		Director dDiegoHallivis = new Director("Diego Hallivis");
 		this.repositoryD.save(dAngelManuel);
@@ -47,19 +47,19 @@ public class DatabaseLoader implements CommandLineRunner {
 		this.repositoryD.save(dDiegoHallivis);
 		this.repositoryD.save(new Director("Roger Taylor"));
 
-		Studio sCineColorColombia = new Studio("Cine Color Colombia");
+		Studio sCineColor = new Studio("Cine Color Colombia");
 		Studio sCinemaARD = new Studio("Cinema ARD");
 		Studio sDcStudios = new Studio("DC Studios");
-		this.repositoryS.save(sCineColorColombia);
+		this.repositoryS.save(sCineColor);
+		this.repositoryS.save(sCinemaARD);
 		this.repositoryS.save(sDcStudios);
 
 		Integrante intAngelManuel = new Integrante(sDcStudios, dAngelManuel, pBlueBeetle);
 		this.repositoryN.save(intAngelManuel);
-		Integrante intRicRoman = new Integrante(sCinemaARD, dRicRoman, pEscapeBajoFuego);
+		Integrante intRicRoman = new Integrante(sCineColor, dRicRoman, pEscapeBajoFuego);
 		this.repositoryN.save(intRicRoman);
-		Integrante intDiegoHallivis = new Integrante(sCineColorColombia, dDiegoHallivis, pHospitalSangriento);
+		Integrante intDiegoHallivis = new Integrante(sCinemaARD, dDiegoHallivis, pHospitalSangriento);
 		this.repositoryN.save(intDiegoHallivis);
-
-
 	}
+
 }
